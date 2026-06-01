629c997c1a8d00616d902d860cdc04f670a9f178
NoorAzam-1 <azamnoor698@gmail.com>
2026-06-01 11:35:30 +0530

    Add home page section composition

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,25 @@
+import PhilosphySection from "@/components/home/PhilosphySection";
+import FeaturedProducts from "@/components/home/FeaturedProducts";
+import WhyChooseSection from "@/components/home/WhyChooseSection";
+import IngredientsSection from "@/components/home/IngredientsSection";
+import Testimonials from "@/components/home/Testimonials";
+import FounderSection from "@/components/home/FounderSection";
+import BlogSection from "@/components/home/BlogSection";
+import CTASection from "@/components/home/CTASection";
+import HeroSection from "./HeroSection";
+
+export default function Home() {
+  return (
+    <div className="pt-32">
+      <HeroSection />
+      <PhilosphySection />
+      <FeaturedProducts />
+      <WhyChooseSection />
+      <IngredientsSection />
+      <Testimonials />
+      <FounderSection />
+      <BlogSection />
+      <CTASection />
+    </div>
+  );
+}
